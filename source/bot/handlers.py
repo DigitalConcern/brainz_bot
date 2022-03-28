fbb4fb2a04db3f857f60de98a6a1c2e3a2c113e0
DigitalConcern <mihanik1608@gmail.com>
2022-03-29 01:57:29 +0300

    trying to connect database

--- source/bot/handlers.py
+++ source/bot/handlers.py
@@ -20,27 +20,28 @@
 
 # MyBot.bot.register_message_handlers(start, commands=["start"])
 # MyBot.bot.register_message_handlers(send_my_id, commands=["id"])
-from ..objects import bot
-from ..web import services
-import telebot
 
-
-@bot.message_handler(commands=["start"])
-def start(message):
-    user, is_created = services.add_user(
-        tg_id=message.from_user.id,
-        username=message.from_user.username
-    )
-
-    if is_created:
-        bot.send_message(message.from_user.id, "You have successfully registered in the bot!")
-    else:
-        bot.send_message(message.from_user.id, "You are already registered in the bot!")
-
-
-@bot.message_handler(commands=["id"])
-def send_my_id(message):
-    bot.send_message(message.from_user.id,
-                     f"User Id: <b>{message.from_user.id}</b>\n" f"Chat Id: <b>{message.chat.id}</b>",
-                     parse_mode="HTML")
+# from ..objects import bot
+# from ..web import services
+# import telebot
+#
+#
+# @bot.message_handler(commands=["start"])
+# def start(message):
+#     user, is_created = services.add_user(
+#         tg_id=message.from_user.id,
+#         username=message.from_user.username
+#     )
+#
+#     if is_created:
+#         bot.send_message(message.from_user.id, "You have successfully registered in the bot!")
+#     else:
+#         bot.send_message(message.from_user.id, "You are already registered in the bot!")
+#
+#
+# @bot.message_handler(commands=["id"])
+# def send_my_id(message):
+#     bot.send_message(message.from_user.id,
+#                      f"User Id: <b>{message.from_user.id}</b>\n" f"Chat Id: <b>{message.chat.id}</b>",
+#                      parse_mode="HTML")
 
--- source/objects.py
+++ source/objects.py
@@ -1,11 +1,15 @@
 import asyncio
 import logging
 import os
-from multiprocessing import Process
-from django.core.asgi import get_asgi_application
 import uvicorn
 import telebot
 import time
+
+from multiprocessing import Process
+from django.core.asgi import get_asgi_application
+
+from ..objects import bot
+from ..web import services
 
 logging.basicConfig(level=logging.DEBUG)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "based.settings")
@@ -15,7 +19,30 @@ os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
-bot = telebot.TeleBot("5229104005:AAG8gMJJ86I34BBsdkk3sBHf8fB8oVxhIzM")
+
+
+def bot_exec():
+    bot = telebot.TeleBot("5229104005:AAG8gMJJ86I34BBsdkk3sBHf8fB8oVxhIzM")
+
+    @bot.message_handler(commands=["start"])
+    def start(message):
+        user, is_created = services.add_user(
+            tg_id=message.from_user.id,
+            username=message.from_user.username
+        )
+
+        if is_created:
+            bot.send_message(message.from_user.id, "You have successfully registered in the bot!")
+        else:
+            bot.send_message(message.from_user.id, "You are already registered in the bot!")
+
+    @bot.message_handler(commands=["id"])
+    def send_my_id(message):
+        bot.send_message(message.from_user.id,
+                         f"User Id: <b>{message.from_user.id}</b>\n" f"Chat Id: <b>{message.chat.id}</b>",
+                         parse_mode="HTML")
+
+    bot.polling(none_stop=True)
 
 
 class MyServer:
@@ -44,8 +71,8 @@ class MyServer:
 def run_app():
     server = Process(target=MyServer.run)
     server.start()
-    bot.polling(none_stop=True)
-    time.sleep(60)
+    bot_exec()
+    # time.sleep(60)
 
 
 if __name__ == "__main__":

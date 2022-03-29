77ce27bdde833538254b7d3e9c22dc2cd70af33a
den10b <den10b@yandex.ru>
2022-03-29 21:42:58 +0300

    Template done

--- source/objects.py
+++ source/objects.py
@@ -2,8 +2,7 @@ import asyncio
 import logging
 import os
 import uvicorn
-import telebot
-import time
+
 
 from multiprocessing import Process
 from django.core.asgi import get_asgi_application
@@ -12,36 +11,11 @@ logging.basicConfig(level=logging.DEBUG)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "based.settings")
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
+
 # load_dotenv(BASE_DIR / "config" / ".env")
 
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
-bot = telebot.TeleBot("5229104005:AAG8gMJJ86I34BBsdkk3sBHf8fB8oVxhIzM")
-
-
-def bot_exec():
-    from web import services
-
-    @bot.message_handler(commands=["start"])
-    def start(message):
-        user, is_created = services.add_user(
-            tg_id=message.from_user.id,
-            username=message.from_user.username
-        )
-
-        if is_created:
-            bot.send_message(message.from_user.id, "You have successfully registered in the bot!")
-        else:
-            bot.send_message(message.from_user.id, "You are already registered in the bot!")
-
-    @bot.message_handler(commands=["id"])
-    def send_my_id(message):
-        bot.send_message(message.from_user.id,
-                         f"User Id: <b>{message.from_user.id}</b>\n" f"Chat Id: <b>{message.chat.id}</b>",
-                         parse_mode="HTML")
-
-    bot.polling(none_stop=True)
-
 
 class MyServer:
     app = get_asgi_application()
@@ -69,8 +43,8 @@ class MyServer:
 def run_app():
     server = Process(target=MyServer.run)
     server.start()
-    bot_exec()
-    # time.sleep(60)
+    from bot.main import bot
+    bot.polling(none_stop=True)
 
 
 if __name__ == "__main__":

a8aec8e59e466d69afdb53ec00fc23bcb9499847
den10b <den10b@yandex.ru>
2022-04-13 16:36:22 +0300

    tryHard to deploy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import asyncio
 from database import loop_db
 from bot import MyBot
-from admin import admin, root_admin_dialog, answer_dialog, post_dialog
-from user import usr_dialog, start
 
 
 async def main():

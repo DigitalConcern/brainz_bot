d1753fdf32880d17f6eddafee549b76cb8f277c4
den10b <den10b@yandex.ru>
2022-04-14 23:23:07 +0300

    tryHard to deploy

--- source/web/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from . models import User
-from . models import Comment
-
-admin.site.register(User)
-admin.site.register(Comment)
--- source/web/services.py
+++
@@ -1,8 +0,0 @@
-from typing import Tuple, Union
-from web import models
-
-
-def add_user(tg_id: int, username: str) -> Tuple[models.User, bool]:
-    return models.User.objects.get_or_create(
-        tg_id=tg_id, username=username
-    )

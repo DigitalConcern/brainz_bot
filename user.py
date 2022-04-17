1198f4df5f0c467c537bdea70d7c31bb729d9ff0
DigitalConcern <mihanik1608@gmail.com>
2022-04-17 19:10:29 +0300

    try init items

--- user.py
+++ user.py
@@ -215,7 +215,7 @@ question_dialog = Dialog(
 # функция для получения данных из состояний
 async def get_data_programs(dialog_manager: DialogManager, **kwargs):
     return {
-        # 'global_info': "".join(list(await Programs.all().values_list("text", flat=True))),
+        'global_info': "".join(list(await Programs.all().values_list("text", flat=True))),
         'choose_program': dialog_manager.current_context().dialog_data.get("choose_program", None),
         'program_info': dialog_manager.current_context().dialog_data.get("program_info", None),
         'keys': list(await Programs.all().values_list("key", flat=True))
@@ -234,7 +234,7 @@ async def on_program_clicked(c: ChatEvent, select: Select, manager: DialogManage
 # Так как через словарь сделать не получается, нужно через бд, где будет столбец для кого программа
 programs_dialog = Dialog(
     Window(
-        Format("global_info"),
+        Format("{global_info}"),
         Row(Select(
             Format("{item}"),
             items="keys",

25a5d612b68f05f7c1374a4532dc9027a7c9c123
den10b <den10b@yandex.ru>
2022-04-28 18:58:46 +0300

    users delete fix

--- source/based/urls.py
+++ source/based/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     path("programs/create", views.CreateProgView.as_view(), name='create'),
     path("admin/", admin.site.urls),
     path("users/<int:pk>", views.UsersEditView.as_view(), name='update2'),
-    path("users/<int:pk>/delete", views.UsersEditView.as_view(), name='del2'),
+    path("users/<int:pk>/delete", views.UsersDelView.as_view(), name='del2'),
 
 ]

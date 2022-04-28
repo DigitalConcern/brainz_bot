463d77256ccada6fae9f30b7582978ea4fa131d4
den10b <den10b@yandex.ru>
2022-04-28 18:52:04 +0300

    users delete fix

--- source/web/models.py
+++ source/web/models.py
@@ -4,7 +4,6 @@ from django.db import models
 # Зарегситрировавшиеся пользователи добавляются в базу данных
 class ActiveUsers(models.Model):
     user_id = models.IntegerField(primary_key=True)
-    password= models.TextField()
     is_admin = models.BooleanField()
     code_name = models.TextField()
     user_name = models.TextField()
